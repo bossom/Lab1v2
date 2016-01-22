0b0ae4c6e7a38bce2175cf99c09f55f9ea589761
Sanna <sanja470@student.liu.se>
2016-01-22 16:27:55 +0100

    Lagt till böcker

--- flaskapp.py
+++ flaskapp.py
@@ -11,6 +11,16 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/om.html')
+def index():
+    return render_template('om.html')
+
+@app.route('/kontakt.html')
+def index():
+    return render_template('kontakt.html')
+
+
+
 @app.route('/<path:resource>')
 def serveStaticResource(resource):
     return send_from_directory('static/', resource)
