39e4d16bb891d27a2cedf5a893fc02e162d08eeb
Sanna <sanja470@student.liu.se>
2016-02-24 10:18:46 +0100

    Lab 3

--- flaskapp.py
+++ flaskapp.py
@@ -1,29 +1,27 @@
-import os
-from datetime import datetime
-
 import re
-from flask import Flask, request, flash, url_for, redirect, \
-     render_template, abort, send_from_directory, json
+from flask import Flask, request, render_template, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Documents/Skola/TDDD83/formAnwers.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/testdb.db'
 db = SQLAlchemy(app)
 
 
-class formAnswers(db.Model):
-    name = db.Column(db.String(80), primary_key=True)
-    email = db.Column(db.String(80), unique=True)
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement = True)
+    name = db.Column(db.String(80), unique=False)
+    email = db.Column(db.String(80), unique=False)
     message = db.Column(db.String(120), unique=False)
 
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
+    def __init__(self, name, email, message):
+         self.name = name
+         self.email = email
+         self.message = message
 
     def __repr__(self):
-        return '<User %r>' % self.name
+         return '<form %r>' % self.name
 
 
 
@@ -31,32 +29,40 @@ class formAnswers(db.Model):
 def index():
    return render_template('index.html')
 
-def is_email_address_valid(InputEmail):
+def is_email_address_valid(email):
     """Validate the email address using a regex."""
-    if not re.match("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$", InputEmail):
+    if not re.match("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$", email):
         return False
     return True
 
-@app.route('/contact', methods=['GET','POST'])
+@app.route("/contact", methods = ['GET', 'POST'])
 def contact():
-    errors=''
-    if request.method == "GET": # If the request is GET, render the form template.
-        return render_template("contact.html", errors=errors)
+    errors = ''
+    if request.method == 'POST':
+        name = request.form['name'].strip()
+        email = request.form['email'].strip()
+        message = request.form['message'].strip()
+        if not name or not email or not message:
+            errors = "Please enter all fields."
+        if not errors:
+            if not is_email_address_valid(email):
+                errors = errors + "Please enter a valid email."
+        if not errors:
+            indata = {'name' : name,
+                    'email' : email,
+                    'message' : message,
+                    }
+            question = Question(name, email, message)
+            db.session.add(question)
+            db.session.commit()
+            data = Question.query.all()
+            print indata
+
+            return render_template ('success.html', data=data)
+        return render_template('contact.html', errors=errors)
     else:
-        InputName = request.form['InputName'].strip()
-        InputEmail = request.form['InputEmail'].strip()
-        InputMessage = request.form['InputMessage'].strip()
-        if not InputName or not InputEmail or not InputMessage:
-            errors = "Please enter all the fields."
-        if not errors:
-            if not is_email_address_valid(InputEmail):
-                errors = errors + "Please enter a valid email address"
-        if not errors:
-            print(InputName, InputMessage, InputEmail)
-            return "Dina data laddades in korrekt"
-        if errors:
-            return errors
-        return render_template('/contact', errors=errors)
+        data = Question.query.all()
+        return render_template('contact.html', data=data)
 
 
 
